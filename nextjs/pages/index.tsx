a3a9c2e980776f192f48065b2143d7943ef13870
Andreas Amstutz <tullo@users.noreply.github.com>
2021-05-03 14:42:44 +0200

    add dark theme and chakra ui

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -1,5 +1,16 @@
 import Link from "next/link";
 import useSWR from "swr";
+import { useState } from "react";
+
+import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  useColorMode,
+  Switch,
+} from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 
 async function fetcher(url: string) {
   const resp = await fetch(url);
@@ -8,26 +19,38 @@ async function fetcher(url: string) {
 
 function Index(): JSX.Element {
   const { data, error } = useSWR("/api", fetcher, { refreshInterval: 1000 });
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [number, setNumber] = useState<number>(0);
 
   return (
-    <div>
-      <h1>Hello, world!</h1>
-      <p>
+    <Box padding="3">
+      <Switch onChange={toggleColorMode} />
+      <Heading as="h1">Hello, world!</Heading>
+      <Text fontSize="md">
         This is <code>pages/index.tsx</code>.
-      </p>
-      <p>
+      </Text>
+      <Text fontSize="md">
         Check out <Link href="/foo">foo</Link>.
-      </p>
+      </Text>
 
-      <h2>Memory allocation stats from Go server</h2>
+      <Heading as="h2">Memory allocation stats from Go server</Heading>
       {error && (
         <p>
           Error fetching profile: <strong>{error}</strong>
         </p>
       )}
-      {!error && !data && <p>Loading ...</p>}
+      {!error && !data && <Text fontSize="md">Loading ...</Text>}
       {!error && data && <pre>{data}</pre>}
-    </div>
+      <Heading>My number is {number}</Heading>
+      <Button
+        leftIcon={<AddIcon />}
+        onClick={() => setNumber((number) => number + 1)}
+        variant="solid"
+        colorScheme="blue"
+      >
+        Increment number
+      </Button>
+    </Box>
   );
 }
 
